e28d83f6463db2d1a5be8bb137cd26dac161786d
kuraki158-spec <kuraki158@gmail.com>
2026-07-08 14:04:42 +0900

    Add files via upload

--- api/[[...path]].js
+++ api/[[...path]].js
@@ -15,7 +15,8 @@ const BROWSER_HEADERS = {
 const LEGACY_PATH = {
   '/busstationservice/v2/getBusStationListv2': '/busstationservice',
   '/busstationservice/v2/getBusStationViaRouteListv2': '/busstationservice/route',
-  '/busarrivalservice/v2/getBusArrivalItemv2': '/busarrivalservice'
+  '/busarrivalservice/v2/getBusArrivalItemv2': '/busarrivalservice',
+  '/busrouteservice/v2/getBusRouteStationListv2': '/busrouteservice/station'
 };
 
 // 간단한 XML → JSON 변환 (GBIS 레거시 응답용)
